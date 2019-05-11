053177cea8b8765fde8e643deca5303a0a26d804
zero1898 <zero1898x@gmail.com>
2019-05-11 23:58:23 +0800

    feat: 对params参数进行处理

---
+++ src/helpers/url.ts
@@ -0,0 +1,56 @@
+import { isDate } from './util'
+
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
+}
+
+export function buildUrl(url: string, params?: any): string {
+  if (!params) {
+    return url
+  }
+
+  const parts: string[] = []
+
+  Object.keys(params).forEach(key => {
+    let val = params[key]
+    // 如果value为null或者不存在
+    if (val === null && typeof val === 'undefined') {
+      return
+    }
+    let values: string[] = []
+    if (Array.isArray(val)) {
+      values = val
+      key += '[]'
+    } else {
+      values = [val]
+    }
+    values.forEach(val => {
+      if (isDate(val)) {
+        val = val.toISOString()
+      } else {
+        val = JSON.stringify(val)
+      }
+      parts.push(`${encode(key)}=${encode(val)}`)
+    })
+  })
+
+  let serializedParams = parts.join('&')
+
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+
+    url += (url.includes('?') ? '?' : '&') + serializedParams
+  }
+
+  return url
+}
